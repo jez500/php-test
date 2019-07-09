20c9b051b60529d1235002c17ba0f03a9c2b4adc
Jeremy Graham <jeremy@doghouse.agency>
2019-07-09 00:15:49 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML>
+<!--
+	Identity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Identity by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<section id="main">
+						<header>
+							<span class="avatar"><img src="/images/bender.png" alt="" width="122" height="122" /></span>
+							<h1>PHP TEST</h1>
+							<p><?php echo "PHP Test page"; ?></p>
+
+						</header>
+
+						<footer>
+							<ul class="icons">
+								<li><a href="/info.php" class="icon brands fa-php">PHP Info</a></li>
+								<li><a href="/database.php" class="icon brands fa-github">Database</a></li>
+								<!-- <li><a href="#" class="icon brands fa-facebook-f">Facebook</a></li> -->
+							</ul>
+						</footer>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li></li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script>
+				if ('addEventListener' in window) {
+					window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
+					document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+				}
+			</script>
+
+	</body>
+</html>
